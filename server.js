7adbb3830efb9318be1aae347dca8c0b308c7281
Jessamyn McTwigan <jessamyn.mctwigan@gmail.com>
2018-07-15 14:19:39 -0500

    set up server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express        = require('express');
+const app            = express();
+const bodyParser     = require('body-parser');
+const methodOverride = require('method-override');
+
+require('./db/db');
+const plantControllers = require('./controllers/plantControllers');
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(methodOverride('_method'));
+
+// every route in plant controller starts with /plantControllers
+app.use('/plants', plantControllers);
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
